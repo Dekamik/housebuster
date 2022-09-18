a32adef89de0d44950ab9e3042d7a4ed5fd05cc0
Dennis von Bargen <dennis@vonbargen.se>
2022-09-18 16:59:47 +0200

    Refactored sorting script

--- sort.py
+++ sort.py
@@ -9,26 +9,22 @@ import sys
 import pandas as pd
 
 
-def sort_json(file_path):
-    df = pd.read_json(file_path)
-    df.sort_values(["Totalindex"], ascending=True, inplace=True)
-    df.to_json(file_path)
-
-
-def sort_csv(file_path):
-    df = pd.read_csv(file_path)
-    df.sort_values(["Totalindex"], ascending=True, inplace=True)
-    df.to_csv(file_path)
+def sort(data_frame):
+    data_frame.sort_values(["Totalindex"], ascending=True, inplace=True)
 
 
 if __name__ == "__main__":
     file = sys.argv[1]
 
     if ".csv" in file:
-        sort_csv(file)
+        df = pd.read_csv(file)
+        sort(df)
+        df.to_csv(file)
 
     elif ".json" in file:
-        sort_json(file)
+        df = pd.read_json(file)
+        sort(df)
+        df.to_json(file)
 
     else:
         ext = os.path.splitext(file)[1]
